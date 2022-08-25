3240b8738c47d0fa29f8b1b4648afbcff1cb0b7b
Kumar Harsh <kumar.co19@nsut.ac.in>
2022-08-26 03:56:06 +0530

    Time: 125 ms (71.70%), Space: 46.5 MB (97.74%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-	    unordered_set<int> nums_set(nums.begin(), nums.end());
-	    return nums_set.size() != nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])
+                return true;
+        }
+        return false;
     }
 };
