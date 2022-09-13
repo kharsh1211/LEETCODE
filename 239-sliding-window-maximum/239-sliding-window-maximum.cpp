84288e7d443bab76b86baa33c225bccc212bb6da
Kumar Harsh <kumar.co19@nsut.ac.in>
2022-09-14 03:17:05 +0530

    Time: 872 ms (12.06%), Space: 148.8 MB (28.02%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> v;
+   
+    priority_queue<pair<int,int>> q;
+   
+    for(int i=0;i<nums.size();i++)
+    { 
+       while(!q.empty()&&q.top().second<=i-k)
+       {
+       q.pop();
+       }
+        q.push({nums[i],i});
+        if(i>=k-1)
+        {
+            v.push_back(q.top().first);
+        }
+    }
+    return v;
+
+    
+    }
+};
