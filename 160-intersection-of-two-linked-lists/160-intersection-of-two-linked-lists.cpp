9fea0220348cd1c6baecf12ac0db5f46dda2767c
Kumar Harsh <kumar.co19@nsut.ac.in>
2022-09-19 02:50:20 +0530

    Time: 119 ms (17.79%), Space: 14.5 MB (73.17%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -13,8 +13,17 @@ public:
         ListNode* d2 = headB;
     
         while(d1 != d2) {
-            d1 = d1 == NULL? headB:d1->next;
-            d2 = d2 == NULL? headA:d2->next;
+            if(d1==NULL)
+                d1=headB;
+            else
+                d1=d1->next;
+            
+            if(d2==NULL)
+                d2=headA;
+            else
+                d2=d2->next;
+            //d1 = d1 == NULL? headB:d1->next;
+            //d2 = d2 == NULL? headA:d2->next;
         }
     
         return d1;
