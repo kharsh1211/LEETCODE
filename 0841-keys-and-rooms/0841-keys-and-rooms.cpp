d3bff1d07c9b73089235138f74ce9a8b3f515770
Kumar Harsh <kumar.co19@nsut.ac.in>
2022-12-20 23:06:07 +0530

    Time: 17 ms (60.22%), Space: 10.6 MB (35.11%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) 
+    {
+        int n=rooms.size();
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int curr_room = q.front(); q.pop();
+            vis[curr_room] = 1;
+            for(auto key:rooms[curr_room])
+                if(!vis[key])
+                    q.push(key);
+        }
+
+        for(int i=0; i<n; i++)
+            if(!vis[i]) return false;
+           
+        return true;
+    }
+};
