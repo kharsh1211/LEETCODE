7acd73b7dbcafce6929d84e9ed96e931c9aebedb
Kumar Harsh <kumar.co19@nsut.ac.in>
2023-01-08 00:44:46 +0530

    Time: 310 ms (100.00%), Space: 122.7 MB (25.00%) - LeetHub

---
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.cpp
@@ -0,0 +1,23 @@
+class DataStream {
+    int val, size, l = 0, check;
+public:
+    DataStream(int value, int k) {
+        val = value, size = check = k;
+    }
+    
+    bool consec(int num) {
+        if(num == val)      check--;          // if same value occurs     
+        else                check = size;     // if stream breaks intialize it back to original Value
+        
+        // If there are less than k integers, the condition does not hold true 
+        // For the 1st K calls return False (as specified in the Question)
+        if(++l < size)      return 0;         
+        return check<=0;               // if same value occurs k time then check==0 and if it occurs more then k then check<0
+    }
+};
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream* obj = new DataStream(value, k);
+ * bool param_1 = obj->consec(num);
+ */
