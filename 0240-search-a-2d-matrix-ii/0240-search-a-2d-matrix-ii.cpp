a4be3493303919b7544a33dca828c82ce8a6ea47
Kumar Harsh <kumar.co19@nsut.ac.in>
2023-06-10 15:07:03 +0530

    Time: 380 ms (11.94%), Space: 14.9 MB (20.27%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        
+        for(int i=0;i<m;i++){
+            auto lower=lower_bound(matrix[i].begin(),matrix[i].end(),target);
+            
+            if(lower!=matrix[i].end() && *lower==target){
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
