febdca65f94ada4d5f1fde1f7e2a153fc65ac093
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-07-31 04:05:36 +0530

    Time: 227 ms (6.24%), Space: 22.8 MB (5.35%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int i,sum=0,j=nums[0],curr=0;
-        int n=nums.size();
-        for(i=0;i<n-1;i++){
-            j=max(j,nums[i]+i);
-            if(i==curr){
-                curr=j;
-                sum++;
+    int solve(vector<int>& nums,int i,vector<int>&dp){
+        if(i>=nums.size()-1) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int jumps=10000001;
+        for(int k=1;k<=nums[i];k++){
+            if (i + k < nums.size()) {
+                jumps=min(jumps,1+solve(nums,i+k,dp));
             }
         }
-        return sum;
+        return dp[i]= jumps;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return solve(nums,0,dp);    
     }
 };
