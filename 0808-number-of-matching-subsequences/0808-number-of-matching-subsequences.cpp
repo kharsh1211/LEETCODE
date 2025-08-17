ebdb5d8f6aa02d7281bf94975ee03fd82d5e0741
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-08-18 01:54:27 +0530

    Time: 57 ms (90.66%), Space: 41.1 MB (97.14%) - LeetHub

---
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isSubsequence(string &s1, string &s2, int m, int n){
+        int j = 0;
+        for(int i = 0; i < m && j < n; i++){
+            if(s1[i] == s2[j]) j++;
+        }
+        return j == n;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+    int n = words.size();
+    int ans = 0;
+    unordered_map<string, int> mp;
+    for(int i = 0; i < n; i++)
+    {
+        if(mp.find(words[i]) != mp.end())
+        {
+            ans += mp[words[i]];
+        }
+        else
+        {
+            mp[words[i]] = isSubsequence(s, words[i], s.length(), words[i].length());
+            ans += mp[words[i]];
+        }
+    }
+    return ans;
+}
+};
