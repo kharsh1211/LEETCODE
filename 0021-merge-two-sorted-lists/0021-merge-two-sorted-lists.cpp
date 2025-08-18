5c696f8a22552d712e54d2504acca68349b5ec09
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-08-18 17:19:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (87.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,37 +11,41 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)
+        if(list1==NULL){
             return list2;
-        if(list2==NULL)
+        }  
+        if(list2==NULL){
             return list1;
-        
-        ListNode*ans=new ListNode(NULL);
-        ListNode*dummy=ans;
-        
-        while(list1!=NULL && list2!=NULL){
-            if(list1->val<list2->val){
-                dummy->next=list1;
-                list1=list1->next;
+        }  
+        ListNode*ans=new ListNode(0);
+        ListNode*temp=ans;
+
+        ListNode*curr1=list1;
+        ListNode*curr2=list2;
+
+        while(curr1!=NULL && curr2!=NULL){
+            if(curr1->val > curr2->val){
+                temp->next=curr2;
+                curr2=curr2->next;
+                temp=temp->next;
             }
             else{
-                dummy->next=list2;
-                list2=list2->next;
+                temp->next=curr1;
+                curr1=curr1->next;
+                temp=temp->next;
             }
-            dummy=dummy->next;
         }
-        
-        while(list1!=NULL){
-            dummy->next=list1;
-            list1=list1->next;
-            dummy=dummy->next;
+        while(curr1!=NULL){
+            temp->next=curr1;
+            curr1 = curr1->next;
+            temp=temp->next;
         }
-        
-        while(list2!=NULL){
-            dummy->next=list2;
-            list2=list2->next;
-            dummy=dummy->next;
+        while(curr2!=NULL){
+            temp->next=curr2;
+            curr2=curr2->next;
+            temp=temp->next;
         }
         return ans->next;
+
     }
 };
