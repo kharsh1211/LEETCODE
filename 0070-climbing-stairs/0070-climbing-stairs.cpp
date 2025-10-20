dee37a9c6223e434500eb47a62b5b137056ab617
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-10-20 13:02:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int solve(int n,int i,vector<int>&dp){
-        if(i==n) return 1;
         if(i>n) return 0;
+        if(i==n) return 1;
 
         if(dp[i]!=-1) return dp[i];
 
