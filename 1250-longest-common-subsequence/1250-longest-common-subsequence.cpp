70f1e31949c53a6294978a259223b2c6c30b5c93
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-10-24 12:00:35 +0530

    Time: 47 ms (11.11%), Space: 27.7 MB (15.31%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int t[1001][1001];
+    int solve(string &text1, string &text2, int i, int j, vector<vector<int>> &dp) {
+        if (i == text1.size() || j == text2.size()) return 0;
 
-    int solve(const string& text1, const string& text2, int i, int j) {
-        if (i >= text1.size() || j >= text2.size()) return 0;
-        if (t[i][j] != -1) return t[i][j];
+        if (dp[i][j] != -1) return dp[i][j];
 
-        if (text1[i] == text2[j])
-            return t[i][j] = 1 + solve(text1, text2, i + 1, j + 1);
-
-        int skip1 = solve(text1, text2, i, j + 1);
-        int skip2 = solve(text1, text2, i + 1, j);
-
-        return t[i][j] = max(skip1, skip2);
+        int ans=0;
+        if(text1[i] == text2[j]) {
+            ans = 1 + solve(text1, text2, i+1, j+1, dp);
+        }
+        else {
+            ans = max(solve(text1, text2, i+1, j, dp), solve(text1, text2, i, j+1, dp));
+        }
+        return dp[i][j]=ans;
     }
 
     int longestCommonSubsequence(string text1, string text2) {
-        memset(t, -1, sizeof(t));
-        return solve(text1, text2, 0, 0);
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(text1, text2, 0, 0, dp);
     }
 };
