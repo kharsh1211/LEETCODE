5a7f6fb485a387f0df3828ff55bc350669b6c36d
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-12-03 20:50:48 +0530

    Time: 0 ms (100%), Space: 10.5 MB (86.1%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        queue<int> q;
+        vector<bool> visited(s.length() + 1, false);
+        
+        q.push(0);
+        visited[0] = true;
+        
+        while (!q.empty()) {
+            int start = q.front();
+            q.pop();
+            
+            if (start == s.length()) return true;
+            
+            for (const string& word : wordDict) {
+                int len = word.length();
+                if (start + len <= s.length()) {
+                    if (s.substr(start, len) == word) {
+                        if (!visited[start + len]) {
+                            visited[start + len] = true;
+                            q.push(start + len);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+};
