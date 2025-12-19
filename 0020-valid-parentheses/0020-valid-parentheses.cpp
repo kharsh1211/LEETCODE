1150adf0d9d9b9c2f18beb0941d1e1d4aa0efe26
Kumar Harsh <kumar.co19@nsut.ac.in>
2025-12-19 10:31:49 +0530

    Time: 0 ms (100%), Space: 9 MB (20.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,16 +5,21 @@ public:
         stack<char>st;
         if(n==1) return false;
         for(int i=0;i<n;i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                st.push(s[i]);
-            } else {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else if(s[i]==')'){
                 if(st.empty()) return false;
-                if((s[i] == ')' && st.top() != '(') ||
-                   (s[i] == '}' && st.top() != '{') ||
-                   (s[i] == ']' && st.top() != '[')) {
-                    return false;
-                }
-                st.pop();
+                else if(st.top()=='(') st.pop();
+                else return false;
+            }
+            else if(s[i]=='}'){
+                if(st.empty()) return false;
+                else if(st.top()=='{') st.pop();
+                else return false;
+            }
+            else if(s[i]==']'){
+                if(st.empty()) return false;
+                else if(st.top()=='[') st.pop();
+                else return false;
             }
         }
         return st.empty();
