b4bb0be54f5ace9b0a4471ecc73298ebee287554
Kumar Harsh <kumar.co19@nsut.ac.in>
2026-01-05 14:24:01 +0530

    Time: 4 ms (25.97%), Space: 13.5 MB (32.46%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -3,13 +3,15 @@ public:
     vector<vector<int>> adj;
     vector<bool> visited;
 
-    bool dfs(int src, int target) {
-        if (src == target) return true;
-        visited[src] = true;
-        
-        for (int neighbor : adj[src]) {
+    bool hasCycle(int node, int parent) {
+        visited[node] = true;
+
+        for (int neighbor : adj[node]) {
             if (!visited[neighbor]) {
-                if (dfs(neighbor, target)) return true;
+                if (hasCycle(neighbor, node)) return true;
+            }
+            else if (neighbor != parent) {
+                return true;
             }
         }
         return false;
@@ -18,21 +20,21 @@ public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
         adj.resize(n + 1);
-        
+
         for (auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            
-            visited.assign(n + 1, false);
-            
-            if (dfs(u, v)) {
-                return edge;
-            }
-            
+
             adj[u].push_back(v);
             adj[v].push_back(u);
+
+            visited.assign(n + 1, false);
+
+            if (hasCycle(u, -1)) {
+                return edge;
+            }
         }
-        
+
         return {};
     }
 };
