2561e26a16b07bde6f50200b8b5abfa245cf4ac5
Kumar Harsh <kumar.co19@nsut.ac.in>
2026-01-07 23:22:41 +0530

    Time: 216 ms (85.29%), Space: 292.1 MB (66.31%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        int m=edges.size();
+        vector<vector<pair<int,int>>>grid(n);
+
+        for(auto &x:edges){
+            int u=x[0];
+            int v=x[1];
+            int w=x[2];
+
+            grid[u].push_back({v,w});
+            grid[v].push_back({u,2*w});
+        } 
+        vector<int>vis(n,0);
+        vector<int>dist(n,1e9);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        pq.push({0,0});
+        dist[0]=0;
+
+        while(!pq.empty()){
+            int node=pq.top().second;
+            int w=pq.top().first;
+            pq.pop();
+
+            if(node==n-1) return w;
+            if(vis[node]) continue;
+            vis[node] = 1;
+
+            for(auto &x:grid[node]){
+                if(!vis[x.first] && dist[x.first]>w+x.second){
+                    dist[x.first]=w+x.second;
+                    pq.push({dist[x.first],x.first});
+                }
+            }
+        }
+        return -1;   
+    }
+};
