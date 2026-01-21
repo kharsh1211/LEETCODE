69f831e73f3326d7d0e5af657f898f54fb56ef21
Kumar Harsh <kumar.co19@nsut.ac.in>
2026-01-21 21:30:48 +0530

    Time: 2 ms (24.36%), Space: 11.4 MB (7.33%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int solve(int i,string s,int n,vector<int>&dp){
-        if(i==n) return 1;
+    int dp[101];
+    int solve(string s,int i){
+        if(i==s.size()) return 1;
         if(s[i]=='0') return 0;
-
         if(dp[i]!=-1) return dp[i];
-
-        int take1=solve(i+1,s,n,dp);
-        int take2=0;
-        if(i+1<n){
-            if(s[i]=='1' || (s[i]=='2' && s[i+1]<='6')){
-                take2=solve(i+2,s,n,dp);
-            }
+        int a=solve(s,i+1);
+        int b=0;
+        if(s[i]!=0 && i+1<s.size() && stoi(s.substr(i,2))<=26){
+            b=solve(s,i+2);
         }
-        return dp[i]=take1+take2;
+        return dp[i]=a+b;
 
     }
     int numDecodings(string s) {
-        int n=s.size();
-        vector<int>dp(n,-1);
-        return solve(0,s,n,dp);   
+        memset(dp,-1,sizeof(dp));
+        return solve(s,0);
+
     }
 };
