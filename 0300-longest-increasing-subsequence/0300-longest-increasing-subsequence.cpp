c8a005f4683f12abed1d318f36b6d8cf06637059
Kumar Harsh <kumar.co19@nsut.ac.in>
2026-02-05 00:32:14 +0530

    Time: 0 ms (100%), Space: 14 MB (88.16%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int dp[3001][3001];
-    int solve(vector<int>& nums,int i,int prev){
-        if(i==nums.size()) return 0;
-
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1]; 
-
-        int pick=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            pick=1+solve(nums,i+1,i);
-        }
-        int notpick=solve(nums,i+1,prev);
-
-        return dp[i][prev+1]=max(pick,notpick);
-
-    }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,-1);    
+        int n=nums.size();
+        vector<int>lis;
+        for(int i=0;i<n;i++){
+            if(lis.empty()||lis.back()<nums[i]) lis.push_back(nums[i]);
+            else{
+                auto it=lower_bound(lis.begin(),lis.end(),nums[i]);
+                *it=nums[i];
+            }
+        }
+        return (int)lis.size();
     }
 };
